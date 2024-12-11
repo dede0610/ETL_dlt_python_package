5f849877f1b62cf6c858901bf9e9633a982f8012
dede0610 <dorian.dessa@gmail.com>
2024-12-11 15:12:57 +1100

    first commit of the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ETL_dlt_python_package
-Try a Python ETL Package. Connect to the chess.com API to fetch result of some players.
+Try a Python ETL Package called dlt. 
+Purpose of this project : Connect to the chess.com API to fetch result of some players 
---
+++ chess_pipeline.py
@@ -0,0 +1,71 @@
+import dlt
+from datetime import date
+from dlt.sources.helpers import requests
+
+today_date = date.today()
+
+
+
+# Create a dlt pipeline that will load
+# chess player data to the DuckDB destination
+pipeline = dlt.pipeline(
+    pipeline_name='chess_pipeline',
+    destination='duckdb',
+    dataset_name='player_data'
+)
+
+
+
+structured_data = []
+
+for player in ['dede0610', 'Fred3337']:
+    
+    url = f'https://api.chess.com/pub/player/{player}'
+    
+    try:
+        # Récupération du profil
+        profile_response = requests.get(url)
+        profile_response.raise_for_status()
+        player_profile = profile_response.json()
+
+        # Récupération des stats
+        stats_response = requests.get(url + '/stats')
+        stats_response.raise_for_status()
+        player_stats = stats_response.json()
+
+
+        # Combine les données pour ce joueur
+        player_record = {
+
+            "username": player_profile["username"],
+            "player_id": player_profile["player_id"],
+            "date_extraction" : today_date,
+            "country": player_profile["country"],
+            "last_online": player_profile["last_online"],
+            "joined": player_profile["joined"],
+            "league": player_profile["league"],
+
+            "chess_rapid_rating": player_stats["chess_rapid"]["last"]["rating"],
+            "chess_rapid_best_rating": player_stats["chess_rapid"]["best"]["rating"],
+            "chess_rapid_best_rating_date": player_stats["chess_rapid"]["best"]["date"],
+            "chess_rapid_stats": player_stats["chess_rapid"]["record"],
+
+            "chess_blitz_rating": player_stats["chess_blitz"]["last"]["rating"],
+            "chess_blitz_best_rating": player_stats["chess_blitz"]["best"]["rating"],
+            "chess_blitz_best_rating_date": player_stats["chess_blitz"]["best"]["date"],
+            "chess_blitz_stats": player_stats["chess_blitz"]["record"],   
+        }
+
+        # Ajoute les données structurées
+        structured_data.append(player_record)
+   
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch data for {player}: {e}")
+
+
+
+# Extract, normalize, and load the data
+pipeline.run(structured_data, table_name='player_stats')
+
+# Terminal command to run to open the streamlit interface with the chess data of the two playes
+# dlt pipeline chess_pipeline show
